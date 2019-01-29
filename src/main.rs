837f5c3a7087427d30f28d625b1e71090a259251
lucas amaral <lucasthund3r@gmail.com>
2019-01-29 22:58:51 +0000

    arg 0 is filename.torrent

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::str::Utf8Error;
-
-use serde_json::{Result, Value};
+use std::env;
 
 fn main() {
-    let mut torrent_file = match File::open("") {
-        Ok(file) => file,
-        Err(_err) => panic!("cannot open file"),
-    };
+    let file_name = env::args().nth(0).unwrap();
+    let mut torrent_file = File::open(file_name).unwrap();
 
     let mut file_content:Vec<u8> = Vec::new();
     torrent_file.read_to_end(& mut file_content);
     let mut file_content_utf8 = String::from_utf8(file_content).unwrap_err();
 
     println!("{}", file_content_utf8.utf8_error().valid_up_to());
-    /*let v: Value = match serde_json::from_slice(file_content.as_slice()) {
-      Ok(k) => k,
-      Err(_e) => panic!(_e),
-    };
-    println!("{}", v["comment"]); */
 }
